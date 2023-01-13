3dc7206ad7429f0b23023fbc1527ba558011608c
Clarisa-Rainet <82967363+Clarisa-Rainet@users.noreply.github.com>
2023-01-13 12:34:25 -0300

    numero whats

--- src/components/aplicativo/index.js
+++ src/components/aplicativo/index.js
@@ -8,8 +8,7 @@ export default function Page(props) {
       <div className="fixed right-6 md:right-2 bottom-10 md:bottom-8 whats-message">
         <a
           target="_blank"
-          href="https://api.whatsapp.com/send?phone=5519974139043"
-        >
+          href="https://api.whatsapp.com/send?phone=5519996965525" >
           <img
             className="flex items-start md:items-end mr-2 md:mr-28 whatsapp"
             src={whats}
